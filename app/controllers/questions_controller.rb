a035b3abda2a6fb5cc7601e83900082b4946bbe5
Mohit Rao <mohitrao.87@gmail.com>
2020-08-10 14:25:06 +0200

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+
+  def answer
+    @ask = params[:ask]
+    @answers =
+      if @ask == "I am going to work!"
+        "Great!"
+      elsif @ask.ends_with?("?")
+        "Silly question, get dressed and go to work!."
+      else
+        "I don't care, get dressed and go to work!"
+    end
+  end
+end
